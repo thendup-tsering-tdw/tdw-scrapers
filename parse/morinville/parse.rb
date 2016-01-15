80b722b1dfe8a742cf6d827ecde09716d9399fd3
Thendup Tsering <thendup11@gmail.com>
2016-01-15 00:53:58 -0500

    Initial Commit- Morinville Data Pull

---
+++ parse/morinville/parse.rb
@@ -0,0 +1,3 @@
+require '../../header.rb'
+
+puts "THendup"
---
+++ pull/morinville/get.rb
@@ -0,0 +1,30 @@
+require '../../header.rb'
+
+shardIdx = ARGV[0].to_i
+shardSize = ARGV[1].to_i
+shardStart = (shardIdx) * shardSize
+shardEnd = shardStart + shardSize - 1
+
+a = Mechanize.new
+loginUrl = 'http://maps.morinville.ca/Morinville/logon.aspx'
+baseUrl = 'http://maps.morinville.ca/Morinville/Views/SearchExport.aspx?type=Report&REPORTTYPE=221&FILEPATH=3210'
+endUrl = '2014_110.PDF&mapName=Morinville'
+
+a.get(loginUrl) do |page|
+  # Click the login link
+  login_page = a.click(page.link_with(:text => /Click here to enter the Public Site/))
+  (shardStart..shardEnd).each do |i|
+    idx = i.to_s.rjust(5, '0')
+    url = "#{baseUrl}#{idx}#{endUrl}"
+    a.get(url) do |pdfFile|
+      if pdfFile.body.match(/Failed to download file using the specified path/)
+        STDERR.puts "File: 3210#{idx}2014_110.pdf not found"
+      else
+        ap "Saved: 3210#{idx}2014_110.pdf"
+        File.open("#{idx}.pdf", "wb") do |file|
+          file.write(pdfFile.body)
+        end
+      end
+    end
+  end
+end
